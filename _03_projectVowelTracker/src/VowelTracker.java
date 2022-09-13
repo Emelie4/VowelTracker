67b27935a7ba815c65b9c7ccf417ff3c37588f86
Chiemelie <Chiozichinweuba@gmail.com>
2022-09-13 09:39:16 -0500

    The beginning. All it does is count the number of vowels and prints that out. But it won't print if the vowel is in upper case

---
+++ _03_projectVowelTracker/src/VowelTracker.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class VowelTracker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * A project to count the number of vowels in a user types line
+		 * More functionality coming
+		 * 
+		 * have it turn all letters to lowercase before starting or account for lower case 
+		 * have it count the different vowels
+		 * have it print that out 
+		 * have it print out the vowel that get used to most
+		 * have it print out the usage of the other letters
+		 * 
+		 */
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Have you ever wondered which vowel gets used the most? Maybe you already know but want to see for yourself.\n"
+				+ "Introducing vowel tracker. Simply type your line and then the program will idenity the precise numbers of a,e,i,o,and u's.\n"
+				+ "Alright get typing");
+		
+		String userVowel = scn.nextLine();
+		int count = 0;
+		for(int v = 0; v < userVowel.length(); v++) {
+			if(userVowel.charAt(v)=='a' || userVowel.charAt(v)=='e' || userVowel.charAt(v)=='i' || userVowel.charAt(v)=='o' || userVowel.charAt(v)=='u') {
+				count++;
+				
+		
+			}//end of if statement 
+		}//end of userVal for loop
+		
+		System.out.println(count);
+	
+	}//end of main
+
+}//end of class
